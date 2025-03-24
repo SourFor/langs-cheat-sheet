ec5ae9f53b9daca9843c77848941fb2b84578365
Aleksandr Kiselev <kiselev.a11@pf5et07c.foreman-test.vm.prod-5.cloud.dataline>
2025-03-25 00:18:30 +0300

    fix generator

--- linux/fingerprint_scaner/info.md
+++ linux/fingerprint_scaner/info.md
@@ -1,6 +1,9 @@
 # Set fingerprint scaner auth Ubuntu
 
 ```sh
+
+sudo apt install fprintd
+
 sudo apt install libpam-fprintd
 
 sudo pam-auth-update
--- linux/scripts/str_and_b64_generator.py
+++ linux/scripts/str_and_b64_generator.py
@@ -5,10 +5,39 @@ import random
 import string
 alphabet_digits_list = list(string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation)
 # print(alphabet_digits_list)
-chars = []
-for i in range(32):
-    chars.append(random.choice(alphabet_digits_list))
-password = "".join(chars)
+
+def generate_pass():
+    chars = []
+    for i in range(32):
+        chars.append(random.choice(alphabet_digits_list))
+    password = "".join(chars)
+    if check_pass(password):
+        return password
+    else:
+        return 0
+
+def check_pass(password):
+    lowercase_flag = False
+    uppercase_flag = False
+    digits_flag = False
+    punct_flag = False
+    for i in password:
+        if not lowercase_flag and i in string.ascii_lowercase:
+            lowercase_flag = True
+            print("lowercase:", i)
+        elif not uppercase_flag and i in string.ascii_uppercase:
+            uppercase_flag = True
+            print("uppercase:", i)
+        elif not digits_flag and i in string.digits:
+            digits_flag = True
+            print("digit:", i)
+        elif not punct_flag and i in string.punctuation:
+            punct_flag = True
+            print("punct:", i)
+
+    return lowercase_flag and uppercase_flag and digits_flag and punct_flag
+
+password = generate_pass()
 print(password)
 salt = base64.b64encode(bytes(password.encode('utf-8')))
 print(salt.decode())
